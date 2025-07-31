c545a364bf09ad7992027342ec69d414b7a63985
FATEC <mbsoaresinfor@gmail.com>
2025-07-30 22:16:29 -0300

    inicio porjeto

---
+++ src/aula/Aplicacao.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		System.out.println("olá mundo");
+
+	}
+
+}
---
+++ src/aula/Produto.java
@@ -0,0 +1,9 @@
+package aula;
+
+public class Produto {
+
+	String nome;
+	float preco;
+	boolean temEstoque;
+	
+}

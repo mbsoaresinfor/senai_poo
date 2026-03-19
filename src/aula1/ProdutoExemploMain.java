1b2211ac385a463df90b727c4aa491a1c4f28a80
unknown <mbsoares.infor@gmail.com>
2026-03-18 22:16:03 -0300

    aula 4

---
+++ src/aula1/ProdutoExemploMain.java
@@ -0,0 +1,69 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class ProdutoExemploMain {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+
+		Produto[] vetorProd = new Produto[5];
+		int cont = 0;
+		while (true) {
+			if (cont >= vetorProd.length) {
+				System.out.println("vetor cheio");
+				break;
+			}
+			Produto produto = new Produto();
+			vetorProd[cont] = produto;
+			cont++;
+
+			System.out.println("Digite codigo do produto ");
+			produto.codigo = teclado.nextInt();
+			System.out.println("Digite nome do produto ");
+			produto.nome = teclado.next();
+			System.out.println("Digite preço do produto ");
+			produto.preco = teclado.nextFloat();
+			System.out.println("Digite peso do produto ");
+			produto.peso = teclado.nextFloat();
+
+			System.out.println("O nome do produto = " + produto.nome);
+
+			produto.mostrarDescricao();
+			produto.verificarSeProdutoCaro();
+
+			System.out.println("Deseja continuar [s/n]");
+			String op = teclado.next();
+			if (op.equals("n")) {
+				break;
+			}
+
+		}
+
+		// percorrendo todo vetor
+		for (int i = 0; i < vetorProd.length; i++) {
+			Produto p = vetorProd[i];
+			if (p != null) {
+				p.mostrarDescricao();
+			}
+		}
+
+		// buscando no vetor pelo codigo
+		System.out.println("Digite codigo do produto para buscar");
+		int codigo = teclado.nextInt();
+		boolean achou = false;
+		for (int i = 0; i < vetorProd.length; i++) {
+			Produto p = vetorProd[i];
+			if (p != null && p.codigo == codigo) {
+				System.out.println("produto localizado");
+				achou = true;
+			}
+		}
+		if(achou == false) {
+			System.out.println("produto não localizado");
+		}
+
+	}
+
+}

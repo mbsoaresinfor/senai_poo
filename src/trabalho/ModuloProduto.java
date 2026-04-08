71ac4a0908ee1550d133d1cf1eb2b66f73dec4f7
MARCELO <mbsoares.infor@gmail.com>
2026-04-08 09:03:01 -0300

    Update ModuloProduto.java

--- src/trabalho/ModuloProduto.java
+++ src/trabalho/ModuloProduto.java
@@ -3,6 +3,8 @@ package trabalho;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class ModuloProduto {
 	private Scanner teclado = new Scanner(System.in);
 	
@@ -10,6 +12,7 @@ public class ModuloProduto {
 			new ArrayList<Produto>();
 	
 	public void cadastrar() {
+		System.out.println("CADASTRO DE PRODUTO");		
 		System.out.println("Digite codigo ");
 		int codigo = teclado.nextInt();
 		System.out.println("Digite nome ");
@@ -35,14 +38,43 @@ public class ModuloProduto {
 	}
 
 	public void relatorio() {
-
+		System.out.println("RELATÓRIO DE PRODUTOS");
+		for(Produto produto : lista) {
+			System.out.println("Nome: " + produto.getNome());
+			System.out.println("Codigo: " + produto.getCodigo());
+			System.out.println("Preço r$ : " + produto.getPreco());
+			System.out.println("---------------------------------");
+		}
 	}
 
 	public void buscar() {
-
+		System.out.println("BUSCA DE PRODUTO");		
+		System.out.println("Digite codigo do produto: ");
+		int codigo = teclado.nextInt();
+		boolean achou = false;
+		for(Produto produto : lista) {
+			if(codigo == produto.getCodigo()) {
+				System.out.println("Produto localizado...");
+				System.out.println("Nome: " + produto.getNome());
+				System.out.println("Codigo: " + produto.getCodigo());
+				System.out.println("Preço r$ : " + produto.getPreco());
+				achou = true;
+			}
+		}
+		if(achou == false) {
+			System.out.println("Produto não localizado");			
+		}
 	}
 
 	public void remocao() {
-
+		System.out.println("REMOÇÃO DE PRODUTO");		
+		System.out.println("Digite codigo do produto: ");
+		int codigo = teclado.nextInt();
+		boolean resultado = lista.removeIf(obj -> obj.getCodigo() == codigo);
+		if(resultado) {
+			System.out.println("Produdo removido com sucesso");
+		}else {
+			System.out.println("Produdo não localizado");
+		}
 	}
 }

252045f355cb70b37432b8937ba0cc58be7d48b6
MARCELO <mbsoares.infor@gmail.com>
2026-04-15 21:40:51 -0300

    Update Setor.java

--- src/aula1/Setor.java
+++ src/aula1/Setor.java
@@ -1,1 +1,47 @@
 
+package aula1;
+
+import java.util.Objects;
+
+public class Setor {
+
+	private Integer codigo;
+	private String setor;
+	private String descricao;
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Setor other = (Setor) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+	
+	
+	
+}

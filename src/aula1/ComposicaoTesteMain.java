73751f4e70a500bb274a8e6d51ed257b2d77b40b
MARCELO <mbsoares.infor@gmail.com>
2026-04-15 21:41:50 -0300

    Update ComposicaoTesteMain.java

--- src/aula1/ComposicaoTesteMain.java
+++ src/aula1/ComposicaoTesteMain.java
@@ -1,1 +1,27 @@
 
+package aula1;
+
+public class ComposicaoTesteMain {
+
+	public static void main(String[] args) {
+
+		Setor setor = new Setor();
+		setor.setDescricao("eletrodomestico");
+		
+		Produto p1 = new Produto(1);
+	
+		
+		System.out.println(p1.getNome());
+		System.out.println(p1.getSetor().getDescricao() );
+		
+		Setor novoSetor = new Setor();
+		novoSetor.setDescricao("descricao alterada");
+		p1.setSetor(novoSetor);
+		p1.setNome("tv alterado");
+		System.out.println(p1.getNome());
+		System.out.println(p1.getSetor().getDescricao() );
+		
+
+	}
+
+}

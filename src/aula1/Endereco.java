78c2acc57d30ae9aa33b944210c4bcea2436dc84
MARCELO <mbsoares.infor@gmail.com>
2026-04-15 21:40:15 -0300

    Update Endereco.java

--- src/aula1/Endereco.java
+++ src/aula1/Endereco.java
@@ -1,1 +1,42 @@
 
+package aula1;
+
+import java.util.Objects;
+
+public class Endereco {
+
+	private String rua;
+	private Integer numero;
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(numero);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(numero, other.numero);
+	}
+	
+	
+	
+}
